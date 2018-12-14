0bf9a3d77f1c6e9a22c4c247f9b0e1aa7c51dc30
Colin <cburns81592@gmail.com>
2018-12-14 14:33:24 -0500

    Updates

---
+++ precache-manifest.9786d0731727c1b17a77ff6292c1dfa6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a324090c45e39205fcf",
+    "url": "/ClickyCats/static/js/runtime~main.6a324090.js"
+  },
+  {
+    "revision": "2b89d9762be059c752ea",
+    "url": "/ClickyCats/static/js/main.2b89d976.chunk.js"
+  },
+  {
+    "revision": "3ad5f8a614c5e526e94e",
+    "url": "/ClickyCats/static/js/1.3ad5f8a6.chunk.js"
+  },
+  {
+    "revision": "2b89d9762be059c752ea",
+    "url": "/ClickyCats/static/css/main.fb304b0c.chunk.css"
+  },
+  {
+    "revision": "6d9734070af9186f2596f53b474e7311",
+    "url": "/ClickyCats/index.html"
+  }
+];
